361bc5e5e3cd391218a03598c35233f0d4772ee1
padillasam <panarick@gmail.com>
2023-11-22 07:58:37 -0500

    CRUD completo

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('lista/',views.usuariolist,name="usuario_list"),
     path('indexFormulario/',views.indexFormulario,name="index_formulario"),
     path('procesarFormulario/',views.procesarFormulario,name="procesar_formulario"),
-
+    path('editarusuario/<int:id_usuario>',views.editarUsuario,name="editar_usuario"),
+    path("actualizar_usuario/<int:id_usuario>",views.actualizarUsuario, name="actualizar_usuario"),
+    path("eliminar_usuario/<int:id_usuario>",views.eliminarUsuario, name="eliminar_usuario"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -24,7 +24,25 @@ def procesarFormulario(request):
             usuario.save()
             usuario=FormularioUsuario()
         return render(request,"formusuario.html",{"form":usuario,"mensaje":"ok"} )
+
+def editarUsuario(request,id_usuario):
+    usuario=Usuario.objects.filter(id=id_usuario).first()
+    form=FormularioUsuario(instance=usuario)
+    return render(request, "UsuarioEdit.html",{"form":form,"usuario":usuario})
+
+def actualizarUsuario(request,id_usuario):
+    usuario=Usuario.objects.get(pk=id_usuario)
+    form=FormularioUsuario(request.POST,instance=usuario)
+    if form.is_valid():
+       form.save()
+       get_usuarios=Usuario.objects.all()
+       return render(request,"usuariolist.html",{"get_usuarios":get_usuarios})
     
+def eliminarUsuario(request, id_usuario):
+    aprendiz=Usuario.objects.get(pk=id_usuario)
+    aprendiz.delete()
+    usuarios=Usuario.objects.all()
+    return render(request, "usuariolist.html",{"get_usuarios":usuarios})
     #***** Este if funciona si se llenan los inputs
     #***** Porque en el modelo no son nulos ni blanks
     #****** eejmplo:  my_field = models.CharField(blank=True, null=True, length=500)
